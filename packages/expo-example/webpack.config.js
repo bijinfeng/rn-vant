ffb6962c4e4bf9319dea66463351cc7645a0afa0
bijinfeng <bijinfeng@bytedance.com>
2021-12-02 13:12:55 +0800

    fix: 删除多余的symlinks
    Change-Id: I581f8b812f51712439d7d7e587c475ca15dfefbb

--- packages/expo-example/webpack.config.js
+++ packages/expo-example/webpack.config.js
@@ -2,15 +2,7 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const path = require('path');
 
 module.exports = async (env, argv) => {
-  const config = await createExpoWebpackConfigAsync(
-    {
-      ...env,
-      babel: {
-        dangerouslyAddModulePathsToTranspile: ['dice-ui'],
-      },
-    },
-    argv
-  );
+  const config = await createExpoWebpackConfigAsync(env, argv);
 
   config.resolve.alias.react = path.join(path.resolve(__dirname), './node_modules/react');
 
