16f6e2a0b1a5aad70a9da2160275a962b17ec250
bijinfeng <2804673379@qq.com>
2022-05-15 16:42:55 +0800

    chore: add visit statistics

--- packages/rn-vant/.umirc.ts
+++ packages/rn-vant/.umirc.ts
@@ -14,6 +14,13 @@ export default defineConfig({
     apiKey: 'c0a3916beb89f5500d092ab4e19143b6',
     indexName: 'docsearch',
   },
+  headScripts: [{
+    src: 'http://45.94.41.154:8025/tracker.js',
+    async: true,
+    // @ts-ignore
+    'data-ackee-server': 'http://45.94.41.154:8025',
+    'data-ackee-domain-id': '672a6b20-7978-4039-ab84-3c4f9d25e793',
+  }],
   favicon: 'https://img01.yzcdn.cn/vant/logo.png',
   logo: 'https://img01.yzcdn.cn/vant/logo.png',
   themeConfig: {
